f502198bee8bb5a62bfadde9b5007a524cf46b38
unknown <genebrierl@SIOPERGAUD.FR>
2022-09-20 15:48:57 +0200

    fin séance 20/09

--- src/Controller/BienvenueController.php
+++ src/Controller/BienvenueController.php
@@ -9,10 +9,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class BienvenueController extends AbstractController
 {
     #[Route('/bienvenue', name: 'app_bienvenue')]
-    public function index(): Response
+    public function bienvenue(): Response
     {
-        return $this->render('bienvenue/index.html.twig', [
-            'nom' => 'Pierre',
+        return $this->render('bienvenue/index.html.twig');
+    }
+
+    #[Route('/bienvenue/{nom}', name: 'app_bienvenue_personne')]
+    public function bienvenuePersonne($nom): Response
+    {
+        return $this->render('bienvenue/bienvenue-personne.html.twig', [
+            'nom' => $nom,
+        ]);
+    }
+
+    #[Route('/bienvenus', name: 'app_bienvenus')]
+    public function bienvenus(): Response
+    {
+        $prenoms = ["Liam", "Paul", "Tiffany"];
+        return $this->render('bienvenue/bienvenus.html.twig', [
+            'prenoms' => $prenoms,
         ]);
     }
 }
