4a0e216eedd1663f6bef010340068e8dd30253c4
unknown <genebrierl@SIOPERGAUD.FR>
2022-09-26 11:51:18 +0200

    fin séance 26/09

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,21 +10,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     // Création d'un objet ArticleRepository par symfony à l'appel de la méthode index()
+    // INJECTION DE DEPENDANCES
+    private ArticleRepository $articleRepository;
+
+    // Demande à symfony d'injecter une instance de ArticleRepository
+
+    /**
+     * @param ArticleRepository $articleRepository
+     */
+    public function __construct(ArticleRepository $articleRepository)
+    {
+        $this->articleRepository = $articleRepository;
+    }
+
+
     #[Route('/articles', name: 'app_articles')]
-    public function index(ArticleRepository $repository): Response
+    public function index(): Response
     {
         //dd($repository->findAll());
         return $this->render('article/index.html.twig', [
-            'articles' => $repository->findBy([], ['createdAt' => 'DESC'], 5),
+            'articles' => $this->articleRepository->findBy([], ['createdAt' => 'DESC']),
         ]);
     }
 
-    #[Route('/article/{id}', name: 'app_article_detail')]
-    public function detail($id, ArticleRepository $repository): Response
+    #[Route('/article/{slug}', name: 'app_article_slug')]
+    public function detail($slug): Response
     {
-        //dd($repository->findAll());
+        //dd( $this->articleRepository->findBy(['slug' => $slug]));
         return $this->render('article/detail.html.twig', [
-            'articles' => $repository->find($id),
+            'article' => $this->articleRepository->findOneBy(['slug' => $slug]),
         ]);
     }
+
 }
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -6,20 +6,32 @@ use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ArticleFixtures extends Fixture
 {
+    private SluggerInterface $slugger;
+
+    /**
+     * @param SluggerInterface $slugger
+     */
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
     public function load(ObjectManager $manager): void
     {
 
         // Faker
         $faker = Factory::create("fr_FR");
 
-        for ($i=0; $i<50; $i++) {
+        for ($i=0; $i<100; $i++) {
             $article = new Article();
             $article->setTitre($faker->words($faker->numberBetween(3,10), true))
                     ->setContenu($faker->paragraphs(3, true))
-                    ->setCreatedAt($faker->dateTimeBetween('-6 months'));
+                    ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                    ->setSlug($this->slugger->slug($article->getTitre())->lower());
             $manager->persist($article); // ordre INSERT
         }
 
